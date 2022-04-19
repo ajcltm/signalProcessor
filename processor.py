821884d049fc0f44a779cdc4b41497a6b7c5bde4
ajcltm <ajcltm@gmail.com>
2022-04-20 00:29:51 +0900

    2022.4.20. 00:19

--- processor.py
+++ processor.py
@@ -13,3 +13,29 @@ class Processor:
     
     def execute(self):
         self.emitter.emit()
+
+
+if __name__ == '__main__':
+    import sys
+    parentPath='c:/Users/ajcltm/PycharmProjects' # parent 경로
+    sys.path.append(parentPath) # 경로 추가
+    from datetime import datetime
+    from blinker import signal
+    from signalProcessor import receiver, emitter, processor
+
+    class I:
+        def __init__(self, ticker):
+            self.ticker = ticker
+
+        def strategy(self, sender, **kwargs):
+            idx = kwargs['idx']
+            date = kwargs['date']
+            print(idx, date, self.ticker)
+
+    sp = signal('BTAlgo')
+
+    r = receiver.BTAlgoReceiver(signal=sp, strategy=I('NVDA').strategy)
+    start, end = datetime(2022, 4, 1), datetime(2022, 4, 19)
+    e = emitter.BTAlgoEmitter(signal=sp, start=start, end=end)
+
+    processor.Processor(emitter=e, receiver=r).execute()
